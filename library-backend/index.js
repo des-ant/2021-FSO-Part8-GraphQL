1ec2a4cb1fda70e07aac1d48c11e2d234f978b53
des-ant <57375593+des-ant@users.noreply.github.com>
2021-11-24 23:14:24 +1100

    Implemented mutation editAuthor, which can be used to set a birth year for an author. -ex8.7

--- library-backend/index.js
+++ library-backend/index.js
@@ -124,6 +124,7 @@ const typeDefs = gql`
       author: String!
       genres: [String!]!
     ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `;
 
@@ -163,6 +164,16 @@ const resolvers = {
       }
 
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+      return updatedAuthor;
     },
   },
 };
