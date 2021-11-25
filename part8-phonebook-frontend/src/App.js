f5b68aa91c50a9a1ab4e72aee294f3cb5c027d39
des-ant <57375593+des-ant@users.noreply.github.com>
2021-11-25 11:06:38 +1100

    Implemented the main view of the application, which shows a list of phone numbers. Used hook function useQuery to make queries

--- part8-phonebook-frontend/src/App.js
+++ part8-phonebook-frontend/src/App.js
@@ -1,9 +1,39 @@
-function App() {
+import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_PERSONS = gql`
+query {
+  allPersons {
+    name
+    phone
+    id
+  }
+}
+`
+
+const Persons = ({ persons }) => {
   return (
     <div>
-      Hello World
+      <h2>Persons</h2>
+      {persons.map(p =>
+        <div key={p.name}>
+          {p.name} {p.phone}
+        </div>  
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  return (
+    <Persons persons = {result.data.allPersons}/>
+  )
+}
+
+export default App
