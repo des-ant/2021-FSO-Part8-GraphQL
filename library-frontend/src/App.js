175ab79723b622c625f50b548767b393dee9dd52
des-ant <57375593+des-ant@users.noreply.github.com>
2021-12-01 18:07:30 +1100

    Implemented user login functionality

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,11 +1,69 @@
 
 import React, { useState } from 'react';
+import {  useApolloClient } from '@apollo/client';
 import Authors from './components/Authors';
 import Books from './components/Books';
 import NewBook from './components/NewBook';
+import LoginForm from './components/LoginForm';
+
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null;
+  }
+  return (
+    <div style={{color: 'red'}}>
+    {errorMessage}
+    </div>
+  );
+};
 
 const App = () => {
   const [page, setPage] = useState('authors');
+  const [token, setToken] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const client = useApolloClient();
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 5000);
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <div>
+        <div>
+          <button onClick={() => setPage('authors')}>authors</button>
+          <button onClick={() => setPage('books')}>books</button>
+          <button onClick={() => setPage('login')}>login</button>
+        </div>
+
+        <Notify errorMessage={errorMessage} />
+
+        <Authors
+          show={page === 'authors'}
+        />
+
+        <Books
+          show={page === 'books'}
+        />
+
+        <LoginForm
+          show={page === 'login'}
+          setToken={setToken}
+          setError={notify}
+          setPage={setPage}
+        />
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -13,7 +71,10 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={logout}>logout</button>
       </div>
+
+      <Notify errorMessage={errorMessage} />
 
       <Authors
         show={page === 'authors'}
